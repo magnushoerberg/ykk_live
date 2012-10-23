1eb596ce21d6f13c3804c7224e26b3a976268b8b
Magnus Hörberg <magnus.hoerberg@gmail.com>
2012-10-23 14:13:48 +0200

    basic funcionality, can set nickname and login, list old messages. send notificaiton when new message is shown

---
+++ client/ykk_live.js
@@ -0,0 +1,112 @@
+Messages = new Meteor.Collection('messages');
+PageLoadTime = Date.now();
+////////// Helpers for in-place editing //////////
+
+// Returns an event map that handles the "escape" and "return" key-
+// events on a text input (given by selector) and interprets them
+// as "ok" or "cancel".
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13) {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+  return events;
+};
+// check for notifications support
+if (window.webkitNotifications) {
+  console.log("Notifications are supported!");
+}
+else {
+  console.log('not supported');
+}
+Template.menu.events({
+  "click #show_button": function() {
+    if (window.Notification && window.Notification.permissionLevel() != 'granted') {
+      window.Notification.requestPermission(function(res) {
+        console.log(res);
+      });
+    }
+  }
+});
+
+Meteor.subscribe("messages");
+Meteor.subscribe("userData");
+var messages = Messages.find({}, { sort: {createdAt: -1} });
+messages.observe({
+  added: function(msg) {
+    if (msg.createdAt < PageLoadTime) {
+      return;
+    }
+    if (window.Notification) {
+      var notification = new Notification(msg.user.nick, {
+        body: msg.text,
+        iconUrl: "image.jpg",
+        tag: msg.user.nick
+      });
+      notification.onshow = function(evt) {
+        setTimeout(function() { evt.target.close(); }, 15000);
+      };
+      notification.onclick = function(evt) {
+        notification.close();
+      };
+      notification.onerror = function(evt) {
+        console.log(evt);
+      };
+      console.log(notification);
+      //"image.jpg", msg.user && msg.user.nick, msg.text);
+      notification.show(); // note the show()
+    } else if(navigator.mozNotification) {
+      var notification = navigator.mozNotification.createNotification(
+          msg.user.nick, msg.text, "image.jpg");
+      notification.show();
+    }
+  }
+});
+Template.msg.messages = function() {
+  return messages;
+};
+Template.updateUserNick.nick = function () {
+  if (Meteor.user())
+    return Meteor.user().nick;
+  else
+    return '';
+};
+Template.updateUserNick.events(okCancelEvents('#nick', {
+  ok: function(value) {
+    Meteor.call('updateUserNick', {nick: value});
+  },
+  cancel: function(evt) {
+    evt.target.value = Meteor.user().nick;
+  }
+}));
+Template.menu.greeting = function () {
+  return "Welcome to ykk_live";
+};
+
+Template.msg.events(okCancelEvents('#newMsg', {
+  ok: function (value, evt) {
+    // template data, if any, is available in 'this'
+    console.log(value, evt);
+    Meteor.call('insertMsg', {text: value}, function(err, res) {
+      evt.target.value = null;
+    });
+  },
+  cancel: function(evt) {
+    evt.target.value = null;
+  }
+}));
---
+++ server/publish.js
@@ -0,0 +1,21 @@
+Messages = new Meteor.Collection('messages');
+
+Meteor.publish("messages", function() {
+  return Messages.find({});
+});
+Meteor.publish("userData", function() {
+  return Meteor.users.find({_id: this.userId}, {fields: {'nick': 1}});
+});
+Meteor.methods({
+  insertMsg: function(msg) {
+    msg.text = msg.text;
+    var usr = Meteor.user();
+    msg.createdAt = Date.now();
+    msg.user = usr;
+    var res = Messages.insert(msg);
+    return res
+  },
+  updateUserNick: function(nick) {
+    return Meteor.users.update( Meteor.userId(), {$set: nick})
+  }
+});
